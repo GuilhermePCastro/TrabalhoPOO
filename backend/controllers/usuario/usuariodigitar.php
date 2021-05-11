11aeddbb1911c36f6c126ce85174c0e1de3f5afd
GuilhermePCastro <guibkk@gmail.com>
2021-05-11 19:55:39 -0300

    Ajustes de sessao

--- backend/controllers/usuario/usuariodigitar.php
+++ backend/controllers/usuario/usuariodigitar.php
@@ -3,9 +3,9 @@ include_once "./../../config/db.php";
 
 //Valdiando sessão
 (__DIR__);
-include_once "./../../classes/loginClass.php";
-$login = new Login();
-$login->validaUser();
+include_once "./../../classes/sessaoClass.php";
+$sessao = new Sessao();
+$sessao->validaUser();
 
 (__DIR__);
 include_once "./../../classes/usuarioClass.php";
